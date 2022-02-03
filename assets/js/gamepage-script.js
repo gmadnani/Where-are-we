12ffbfabfdc5ac6a06cc05b7fb89ec0a25e837ef
Daniel Bowen <djbowen95@gmail.com>
2022-02-03 20:25:11 +0000

    describe functions needed in a javascript file for basic game to run effectively

---
+++ assets/js/gamepage-script.js
@@ -0,0 +1,14 @@
+// picContainer, four ansLocation
+
+// Function to retrieve photo. Will replace with API function.
+// Function to generate four answers. Will replace with API function.
+// Function to randomise the order of the four answers.
+// Function to run when correct answer is selected.
+// Function to run when incorrect answer is selected.
+// Function that generates a new question when 
+// Timer that resets with each new question.
+
+// Function to present scores.
+// Function to collect user input / their name.
+// Function to store score and name.
+// Function to restart the game. 
