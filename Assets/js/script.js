a2f5ebe575e25552eec7f32a437090a2d178c50f
Jean McLeod <abricsltd@gmail.com>
2022-02-09 21:06:28 +0000

    got dropdown working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,55 @@
 
+
+
+
+
+/*$(document).ready(function (){
+    $('.group').hide();
+    $('#option5').show();
+    $('#selectMe').change(function () {
+        $('.group').hide();
+        $('#'+$(this).val()).show();
+
+    })
+});*/
+
+
+/*document
+                .getElementById("selectMe")
+                .addEventListener('change', function () {
+                    'use strict';
+                    var vis = document.querySelector('.vis'),   
+                        selectMe = document.getElementById(this.value);
+                    if (vis !== null) {
+                        vis.className = 'inv';
+                    }
+                    if (selectMe !== null ) {
+                        selectMe.className = 'vis';
+                    }
+            });*/
+
+
+
+
 /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+toggle between hiding and showing the dropdown content 
+function Droplist() {
+    document.getElementByClass("dropdown-content").dropList.toggle("show");
   }
-  
+ function selectNumberofQ () {
+     document.getElementById(" ").value;
+ }
+
+
+/*psuedocode
+if an item is clicked then display this item and hide the others
+
+
+
+
+
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+  /*window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
@@ -17,4 +60,4 @@ function myFunction() {
         }
       }
     }
-  }
+  }*/
