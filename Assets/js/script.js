d82b64e63c560e77f33d4ecd9ab462ba5510e9a3
Jean McLeod <abricsltd@gmail.com>
2022-02-10 02:23:38 +0000

    add timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,81 +1,54 @@
 function getSelectValue (){
 var selectedValue = document.getElementById("selectMe").value;
 console.log (selectedValue);
+
+
+/*Duration of time*/
+let moment = durationofGame(selectedValue * 15).seconds
+let durationofGame = moment().format('H:mm:ss').show;
 }
 
+// convert seconds to minutes
+/* OPTION1
+function timeConvert(){
+ let num = document.getElementById('selectedValue').value;
+var hours = Math.floor(num/60);
+var minutes = num % 60;
+var seconds = sec % 600
+document.getElementById("result").innerHTML = 
+hours + ";" +minutes + "<br>" + "(" +hours + "Hours & " + minutes + "Minutes &" + seconds + "Seconds)";
+}*/
+
+/*
+OPTION 2
+var toHHMMSS = (secs) => {
+  var sec_num = parseInt(secs, 10)
+  var hours   = Math.floor(sec_num / 3600)
+  var minutes = Math.floor(sec_num / 60) % 60
+  var seconds = sec_num % 60
+
+  return [hours,minutes,seconds]
+      .map(v => v < 10 ? "0" + v : v)
+      .filter((v,i) => v !== "00" || i > 0)
+      .join(":")
+}*/
 
 
+/* OPTION 3 stackoverflow suggestion by TJ Crowder
+let totalSeconds = 28565;
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+
+console.log("hours: " + hours);
+console.log("minutes: " + minutes);
+console.log("seconds: " + seconds);
+
+// If you want strings with leading zeroes:
+minutes = String(minutes).padStart(2, "0");
+hours = String(hours).padStart(2, "0");
+seconds = String(seconds).padStart(2, "0");
+console.log(hours + ":" + minutes + ":" + seconds);   */     
 
 
-
-/*function isChecked () {
-    var five = document.getElementById('5').checked;
-    var ten = document.getElementById('10').checked;
-    var fifteen = document.getElementById('15').checked;
-    var twenty = document.getElementById('20').checked;
-
-    if (five==false && ten==false && fifteen==false && twenty==false)(
-        alert('please select a timer option'))
-    return false;
-    
-}
-
-
-/*$(document).ready(function (){
-    $('.group').hide();
-    $('#option5').show();
-    $('#selectMe').change(function () {
-        $('.group').hide();
-        $('#'+$(this).val()).show();
-
-    })
-});*/
-
-
-/*document
-                .getElementById("selectMe")
-                .addEventListener('change', function () {
-                    'use strict';
-                    var vis = document.querySelector('.vis'),   
-                        selectMe = document.getElementById(this.value);
-                    if (vis !== null) {
-                        vis.className = 'inv';
-                    }
-                    if (selectMe !== null ) {
-                        selectMe.className = 'vis';
-                    }
-            });*/
-
-
-
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content 
-function Droplist() {
-    document.getElementByClass("dropdown-content").dropList.toggle("show");
-  }
- function selectNumberofQ () {
-     document.getElementById(" ").value;
- }
-
-
-/*psuedocode
-if an item is clicked then display this item and hide the others
-
-
-
-
-
-  // Close the dropdown menu if the user clicks outside of it
-  /*window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }*/
